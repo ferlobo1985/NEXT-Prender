e8806bcddc6bf8e51c95b6c2be20764d6db50501
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-06-07 17:39:00 -0300

    05-Not found and redirect

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,23 @@ const Home = (props) => {
 
 export async function getStaticProps(){
   const request = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+  
+  // if(request.data.length > 0){
+  //   return{
+  //     redirect:{
+  //       destination:'/someplace_else'
+  //     }
+  //     ///notFound:true
+  //   }
+  // }
   
   return{
     props:{
       name:'Francis',
       value:'Just a girl standing in front of a boy...',
       theRequest:request.data
-    },
-    revalidate:60
+    }
   }
 }
 
